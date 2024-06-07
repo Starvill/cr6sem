564f7fd0743f7e3acdd7068251ebdea484780027
Starvil <rublev2606@mail.ru>
2024-06-07 21:31:27 +0300

    removewashtime3

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,8 +4,9 @@ import './Item.css';
 
 const Item = ({ item }) => {
   const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
+    let formattedString = dateString.replace('T', ' ').replace('Z', '');
+    formattedString = formattedString.split('.')[0];
+    return formattedString;
   };
 
   const currentTime = new Date();
